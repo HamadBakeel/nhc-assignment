05ec40831f525a908b94705d308e859c32859e0f
Hamad Bakeel <hamadbakeel@gmail.com>
2024-10-16 23:30:06 +0300

    new: footer and navbar

--- src/@core/layout/footer/index.tsx
+++ src/@core/layout/footer/index.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image"
+
 const Footer=()=>{
 
     return(
-        <footer>
-            {/* Here add footer design  */}
-            Footer
+        <footer className="bg-[#292759] text-white px-28 pt-10 flex flex-col justify-end">
+            <span className="h-[1px] w-full block bg-[#F1F1F1] opacity-20"></span>
+            <div className="flex justify-between items-center py-4">
+                <div className="flex gap-4">
+                    <Image src="nhc-logo-white.svg" alt="" width={32} height={32} />
+                    <Image src="vision-2030-white.svg" alt="" width={50} height={32} />
+                </div>
+
+                <p className="text-sm text-white">
+                    All rights reserved © 2022 - Developed and operated by National Housing
+                </p>
+            </div>
         </footer>
     )
 }
--- src/@core/layout/nav/index.tsx
+++ src/@core/layout/nav/index.tsx
@@ -1,11 +1,39 @@
-const NavBar=()=>{
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
-    return(
+const NavBar = () => {
+    const pathname = usePathname();
+
+    const routes = [
+        {
+            name: 'Products',
+            href: '/products'
+        },
+        {
+            name: 'About',
+            href: '/about'
+        }
+    ]
+    return (
         <nav
-        className="flex flex-row justify-between items-center w-full h-16 px-4 bg-white shadow-sm"
+            className="flex flex-row justify-start items-center w-full h-16 px-8 py-2 bg-[#D9D9D91F] shadow-sm gap-14"
         >
-            navBar
-{/* TODO here put the nav bar item */}
+            <Image src="nhc-logo.svg" alt="" width={60} height={60} />
+
+            <div className="flex justify-between gap-10">
+                {
+                    routes.map((link) => {
+                        const isActive = pathname.startsWith(link.href)
+                        return (<Link href={link.href} className={isActive ? 'text-[#285F9D]' : 'text-black'}>
+                            {link.name}
+                        </Link>
+                        )
+                    })
+                }
+            </div>
         </nav>
     )
 }
