103c160623fd8de68caf405cf476bb6761549936
Hamad Bakeel <hamadbakeel@gmail.com>
2024-10-17 12:37:25 +0300

    fix: footer responsive

--- src/@core/layout/footer/index.tsx
+++ src/@core/layout/footer/index.tsx
@@ -3,15 +3,15 @@ import Image from "next/image"
 const Footer=()=>{
 
     return(
-        <footer className="bg-[#292759] text-white px-28 pt-10 flex flex-col justify-end">
+        <footer className="bg-[#292759] text-white px-28 pt-10 md:flex-col justify-end">
             <span className="h-[1px] w-full block bg-[#F1F1F1] opacity-20"></span>
-            <div className="flex justify-between items-center py-4">
+            <div className="flex flex-col md:flex-row gap-2 justify-between items-center py-4">
                 <div className="flex gap-4">
                     <Image src="/nhc-logo-white.svg" alt="" width={32} height={32} />
                     <Image src="/vision-2030-white.svg" alt="" width={50} height={32} />
                 </div>
 
-                <p className="text-sm text-white font-abel">
+                <p className="text-sm text-white font-abel text-center">
                     All rights reserved © 2022 - Developed and operated by National Housing
                 </p>
             </div>
